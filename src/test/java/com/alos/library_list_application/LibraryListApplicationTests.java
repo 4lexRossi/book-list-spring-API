2f4e43ebf0288c839b29af0076fdaaf1f985b7f2
Alexsander Osmir Rossi <devalexrossi@gmail.com>
2021-08-21 18:50:15 -0300

    Update LibraryListApplicationTests.java

--- src/test/java/com/alos/library_list_application/LibraryListApplicationTests.java
+++ src/test/java/com/alos/library_list_application/LibraryListApplicationTests.java
@@ -1,13 +1,13 @@
-package com.alos.library_list_application;
+// package com.alos.library_list_application;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class LibraryListApplicationTest {
+// @SpringBootTest
+// class LibraryListApplicationTest {
 
-	@Test
-	void contextLoads() {
-	}
+// 	// @Test
+// 	// void contextLoads() {
+// 	// }
 
-}
+// }
